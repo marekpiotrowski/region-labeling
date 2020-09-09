b8c64fe07e884ec96511f6f66769b1cfc6c13074
marekpiotrowski <marek_piotrowski@live.com>
2020-09-09 23:11:35 +0200

    Removed default comments

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 class RangeRegion(object):
     def __init__(self, l, r):
         self.r = range(l, r+1)
@@ -18,7 +7,6 @@ def sum_up_to_continuous_range(range_in_row, range_in_next_row):
     return len(range(min(range_in_row.r[0], range_in_next_row.r[0]),
                      max(range_in_row.r[-1], range_in_next_row.r[-1]))) == len(range_in_row.r) + len(range_in_next_row.r)
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     z = []
     a = [RangeRegion(1,1)]
@@ -45,5 +33,3 @@ if __name__ == '__main__':
 
 
     print(r)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
